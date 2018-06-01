5c862c923f8eda9bf624d2d2b34d47d9d5af249d
lovelife10000 <1358180015@qq.com>
2018-06-01 16:20:20 +0800

    update

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ var a={
 if(a==3 && a==4){
     console.log(666);
 }
+
+
+//输入时间戳获取格式如2018-06-06 00:00:00
+function(timestamp){
+    var date =new Date(timestamp);
+    Y = date.getFullYear() + '-';
+    M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
+    D = (date.getDate() < 10 ? '0'+(date.getDate()) : date.getDate()) + ' ';
+    h = (date.getHours() < 10 ? '0'+(date.getHours()) : date.getHours()) + ':';
+    m = (date.getMinutes() < 10 ? '0'+(date.getMinutes()) : date.getMinutes()) + ':';
+    s = (date.getSeconds() < 10 ? '0'+(date.getSeconds()) : date.getSeconds());
+    return Y+M+D+h+m+s;
+},
